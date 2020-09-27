33337e0f47672488f7eda410e3a79b5dc0983871
andryue <kacperwojak17@gmail.com>
2020-09-27 10:25:26 +0200

    create welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './screens/welcome/welcome.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MedicineApp());
 
-class MyApp extends StatelessWidget {
+class MedicineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        fontFamily: "Popins",
-        primaryColor: Color.fromRGBO(7, 203, 194, 1),
-      ),
-      home: Scaffold(
-        body: Center(
-            child: Text(
-          "Something",
-              style: Theme.of(context).textTheme.bodyText2,
-        )),
-      ),
-    );
+        theme: ThemeData(
+            fontFamily: "Popins",
+            primaryColor: Color.fromRGBO(7, 190, 200, 1),
+            textTheme: TextTheme(
+                headline1: ThemeData.light().textTheme.headline1.copyWith(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 38.0,
+                      fontFamily: "Popins",
+                    ),
+                headline5: ThemeData.light().textTheme.headline1.copyWith(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17.0,
+                      fontFamily: "Popins",
+                    ),
+            headline3: ThemeData.light().textTheme.headline3.copyWith(
+              fontWeight: FontWeight.w600,
+              fontSize: 20.0,
+              fontFamily: "Popins",
+            ))),
+        home: Welcome());
   }
 }
---
+++ lib/screens/welcome/welcome.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double deviceHeight =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: deviceHeight * 0.04,
+            ),
+            Image.asset('assets/images/welcome_image.png',
+                width: double.infinity, height: deviceHeight * 0.4),
+            SizedBox(
+              height: deviceHeight * 0.05,
+            ),
+            Container(
+              height: deviceHeight * 0.15,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+                child: Text(
+                  "Be in control of your meds",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1
+                      .copyWith(color: Colors.black, height: 1.3),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Container(
+              height: deviceHeight * 0.15,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+                child: Text(
+                  "An easy-to-use and reliable app that helps you remember to take your meds at the tight time",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      .copyWith(color: Colors.grey[600], height: 1.3),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: deviceHeight * 0.03,
+            ),
+            Container(
+              height: deviceHeight * 0.09,
+              width: double.infinity,
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 35.0, right: 35.0),
+                  child: FlatButton(
+                    color: Theme.of(context).primaryColor,
+                    child: Text(
+                      "Get started now",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3
+                          .copyWith(color: Colors.white),
+                    ),
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
