92048c2f7c65361b7358630878112648b87ca56b
andryue <kacperwojak17@gmail.com>
2020-09-29 19:40:51 +0200

    setup calendar in layout builder

--- lib/screens/home/calendar_day.dart
+++ lib/screens/home/calendar_day.dart
@@ -14,35 +14,41 @@ class CalendarDay extends StatefulWidget {
 class _CalendarDayState extends State<CalendarDay> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          widget.day.dayLetter,
-          style: TextStyle(
-              color: Colors.grey[500],
-              fontSize: 17.0,
-              fontWeight: FontWeight.w400),
-        ),
-        SizedBox(
-          height: 12.0,
-        ),
-        GestureDetector(
-          onTap: () => widget.onDayClick(widget.day),
-          child: CircleAvatar(
-            backgroundColor: widget.day.isChecked
-                ? Theme.of(context).primaryColor
-                : Colors.transparent,
-            child: Text(
-              widget.day.dayNumber,
-              style: TextStyle(
-                  color: widget.day.isChecked ? Colors.white : Colors.black,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w500),
+    return LayoutBuilder(
+      builder: (context,constrains) => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            widget.day.dayLetter,
+            style: TextStyle(
+                color: Colors.grey[500],
+                fontSize: 17.0,
+                fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: constrains.maxHeight * 0.1,
+          ),
+          GestureDetector(
+            onTap: () => widget.onDayClick(widget.day),
+            child: CircleAvatar(
+              radius: constrains.maxHeight * 0.25,
+              backgroundColor: widget.day.isChecked
+                  ? Theme.of(context).primaryColor
+                  : Colors.transparent,
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Text(
+                  widget.day.dayNumber,
+                  style: TextStyle(
+                      color: widget.day.isChecked ? Colors.white : Colors.black,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
