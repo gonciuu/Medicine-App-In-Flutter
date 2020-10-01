638b420640f6a82850cf683ff47f2ef4f9e2fe59
andryue <kacperwojak17@gmail.com>
2020-10-01 16:06:06 +0200

    create dropdown menu button to select the weight type

--- lib/screens/add_new_medicine/add_new_medicine.dart
+++ lib/screens/add_new_medicine/add_new_medicine.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class AddNewMedicine extends StatefulWidget {
   @override
   _AddNewMedicineState createState() => _AddNewMedicineState();
 }
 
 class _AddNewMedicineState extends State<AddNewMedicine> {
+  String selectWeight;
+  final List<String> weightValues = ["pills", "ml", "mg"];
+
+  int howManyWeeks = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    selectWeight = weightValues[0];
+  }
+
   @override
   Widget build(BuildContext context) {
-
     final deviceHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: Color.fromRGBO(248, 248, 248, 1),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20.0,right: 20.0,top: 30.0),
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -23,41 +34,104 @@ class _AddNewMedicineState extends State<AddNewMedicine> {
                 height: deviceHeight * 0.05,
                 child: FittedBox(
                   child: InkWell(
-                    child: Icon(
-                      Icons.arrow_back
-                    ),
-                    onTap: (){
+                    child: Icon(Icons.arrow_back),
+                    onTap: () {
                       Navigator.pop(context);
                     },
                   ),
                 ),
               ),
-              SizedBox(height: deviceHeight * 0.02,),
+              SizedBox(
+                height: deviceHeight * 0.02,
+              ),
               Container(
                 padding: EdgeInsets.only(left: 15.0),
                 height: deviceHeight * 0.06,
                 child: FittedBox(
-                  child: Text("Add Pills",style: Theme.of(context).textTheme.headline3.copyWith(color: Colors.black),)
-                ),
-              ),SizedBox(height: deviceHeight * 0.03,),
+                    child: Text(
+                  "Add Pills",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3
+                      .copyWith(color: Colors.black),
+                )),
+              ),
+              SizedBox(
+                height: deviceHeight * 0.03,
+              ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal : 10.0),
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
                 child: Column(
                   children: [
-                    TextFormField(
+                    TextField(
                       style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16.0
-                      ),
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16.0),
                       decoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 20.0),
-                        labelText: "Pills Name",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(width:0.5,color: Colors.grey)
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 15.0, vertical: 20.0),
+                          labelText: "Pills Name",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide:
+                                  BorderSide(width: 0.5, color: Colors.grey))),
+                    ),
+                    SizedBox(
+                      height: deviceHeight * 0.03,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          flex: 2,
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16.0),
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 15.0, vertical: 20.0),
+                                labelText: "Pills Amount",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    borderSide: BorderSide(
+                                        width: 0.5, color: Colors.grey))),
+                          ),
+                        ),   SizedBox(
+                          width: 10,
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: DropdownButtonFormField(
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 15.0, vertical: 20.0),
+                                labelText: "Type",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    borderSide: BorderSide(
+                                        width: 0.5, color: Colors.grey))),
+                            items: weightValues
+                                .map((weight) => DropdownMenuItem(
+                                      child: Text(weight),
+                                      value: weight,
+                                    ))
+                                .toList(),
+                            onChanged: (value) =>
+                                setState(() => this.selectWeight = value),
+                            value: selectWeight,
+
+                          ),
                         )
-                      ),
+                      ],
+                    ),
+                    Slider(value: value,
+                      onChanged: (value){},
+                      max: 30,
+                      min: 0,
+
                     )
                   ],
                 ),
