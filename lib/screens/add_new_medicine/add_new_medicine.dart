d74f2e9f501f8cf90585473a7c6d0dd8a16ab0b4
andryue <kacperwojak17@gmail.com>
2020-10-01 17:43:24 +0200

    create slider with how long the medicine will be take

--- lib/screens/add_new_medicine/add_new_medicine.dart
+++ lib/screens/add_new_medicine/add_new_medicine.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:medicine/helpers/platform_slider.dart';
 
 class AddNewMedicine extends StatefulWidget {
   @override
@@ -62,6 +63,7 @@ class _AddNewMedicineState extends State<AddNewMedicine> {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     TextField(
                       style: TextStyle(
@@ -99,7 +101,8 @@ class _AddNewMedicineState extends State<AddNewMedicine> {
                                     borderSide: BorderSide(
                                         width: 0.5, color: Colors.grey))),
                           ),
-                        ),   SizedBox(
+                        ),
+                        SizedBox(
                           width: 10,
                         ),
                         Flexible(
@@ -122,16 +125,32 @@ class _AddNewMedicineState extends State<AddNewMedicine> {
                             onChanged: (value) =>
                                 setState(() => this.selectWeight = value),
                             value: selectWeight,
-
                           ),
                         )
                       ],
                     ),
-                    Slider(value: value,
-                      onChanged: (value){},
-                      max: 30,
-                      min: 0,
-
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: Text("How long?"),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: PlatformSlider(
+                                divisions: 11,
+                                min: 1,
+                                max: 10,
+                                value: howManyWeeks,
+                                color: Theme.of(context).primaryColor,
+                                handler: sliderChanged)),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text('$howManyWeeks weeks'),
                     )
                   ],
                 ),
@@ -142,4 +161,7 @@ class _AddNewMedicineState extends State<AddNewMedicine> {
       ),
     );
   }
+
+  void sliderChanged(double value) =>
+      setState(() => this.howManyWeeks = value.toInt());
 }
