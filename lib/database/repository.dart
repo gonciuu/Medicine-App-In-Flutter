d4499de23da698230c0a76a9e20112e976052a09
andryue <kacperwojak17@gmail.com>
2020-10-08 17:56:05 +0200

    create repository

---
+++ lib/database/repository.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:medicine/database/pills_database.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Repository{
+
+  PillsDatabase _pillsDatabase = PillsDatabase();
+  static Database _database;
+
+  //init database
+  Future<Database> get database async{
+    if(_database != null) return _database;
+    _database = await _pillsDatabase.setDatabase();
+    return _database;
+  }
+
+  Future<int> insertData(String table,Map<String,dynamic> data) async{
+    Database db = await database;
+    return await db.insert(table, data);
+  }
+  
+
+}
