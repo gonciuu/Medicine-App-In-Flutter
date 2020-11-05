a5ae9fa6cad95719b6b85e5684a13bcb9642556f
andryue <kacperwojak17@gmail.com>
2020-11-05 12:07:15 +0100

    show alert dialog on long click

--- lib/screens/home/medicine_card.dart
+++ lib/screens/home/medicine_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:medicine/models/pill.dart';
+import 'package:path/path.dart';
 
 class MedicineCard extends StatelessWidget {
   final Pill medicine;
@@ -11,55 +12,81 @@ class MedicineCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final bool isEnd = DateTime.now().millisecondsSinceEpoch > medicine.time;
     return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      elevation: 0.0,
-      margin: EdgeInsets.symmetric(vertical: 7.0),
-      color: Colors.white,
-      child: ListTile(
-          contentPadding:
-              EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
-          title: Text(
-            medicine.name,
-            style: Theme.of(context).textTheme.headline1.copyWith(
-                color: Colors.black,
-                fontSize: 20.0,
-                decoration: isEnd ? TextDecoration.lineThrough : null),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Text(
-            "${medicine.amount} ${medicine.medicineForm}",
-            style: Theme.of(context).textTheme.headline5.copyWith(
-                color: Colors.grey[600],
-                fontSize: 15.0,
-                decoration: isEnd ? TextDecoration.lineThrough : null),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          trailing: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                DateFormat("HH:mm")
-                    .format(DateTime.fromMillisecondsSinceEpoch(medicine.time)),
-                style: TextStyle(
-                    color: Colors.grey[500],
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15,
-                    decoration: isEnd ? TextDecoration.lineThrough : null),
-              ),
-            ],
-          ),
-          leading: Container(
-            width: 60.0,
-            height: 60.0,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50.0),
-              child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(isEnd ? Colors.white : Colors.transparent, BlendMode.saturation),
-                  child: Image.asset("assets/images/welcome_image.png")),
+        elevation: 0.0,
+        margin: EdgeInsets.symmetric(vertical: 7.0),
+        color: Colors.white,
+        child: ListTile(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            onLongPress: () => _showDeleteDialog(context, medicine.name),
+            contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+            title: Text(
+              medicine.name,
+              style: Theme.of(context).textTheme.headline1.copyWith(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  decoration: isEnd ? TextDecoration.lineThrough : null),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
-          )),
-    );
+            subtitle: Text(
+              "${medicine.amount} ${medicine.medicineForm}",
+              style: Theme.of(context).textTheme.headline5.copyWith(
+                  color: Colors.grey[600],
+                  fontSize: 15.0,
+                  decoration: isEnd ? TextDecoration.lineThrough : null),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat("HH:mm").format(DateTime.fromMillisecondsSinceEpoch(medicine.time)),
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                      decoration: isEnd ? TextDecoration.lineThrough : null),
+                ),
+              ],
+            ),
+            leading: Container(
+              width: 60.0,
+              height: 60.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50.0),
+                child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        isEnd ? Colors.white : Colors.transparent,
+                        BlendMode.saturation),
+                    child: Image.asset("assets/images/welcome_image.png")),
+              ),
+            )));
+  }
+
+  void _showDeleteDialog(BuildContext context, String medicineName) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Delete ?"),
+              content: Text("Are you sure to delete $medicineName medicine?"),
+              contentTextStyle: TextStyle(fontSize: 17.0,color: Colors.grey[800]),
+              actions: [
+                FlatButton(
+                  splashColor: Theme.of(context).primaryColor.withOpacity(0.3),
+                  child: Text("Cancel",style: TextStyle(color: Theme.of(context).primaryColor),),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                FlatButton(
+                  splashColor: Theme.of(context).primaryColor.withOpacity(0.3),
+                  child: Text("Delete",style: TextStyle(color: Theme.of(context).primaryColor)),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
+            ));
   }
 }
